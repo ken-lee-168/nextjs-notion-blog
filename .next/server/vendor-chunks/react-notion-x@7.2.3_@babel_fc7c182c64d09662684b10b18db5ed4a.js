1e3480df85234ba6d72f39cb07259f846c4338a0
lijianwei <lijianwei@dianxiaomi.com>
2025-05-15 19:25:20 +0800

    feat: init;

---
+++ .next/server/vendor-chunks/react-notion-x@7.2.3_@babel_fc7c182c64d09662684b10b18db5ed4a.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/react-notion-x@7.2.3_@babel_fc7c182c64d09662684b10b18db5ed4a";
+exports.ids = ["vendor-chunks/react-notion-x@7.2.3_@babel_fc7c182c64d09662684b10b18db5ed4a"];
+exports.modules = {
+
+/***/ "./node_modules/.pnpm/react-notion-x@7.2.3_@babel_fc7c182c64d09662684b10b18db5ed4a/node_modules/react-notion-x/src/styles.css":
+/*!************************************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/react-notion-x@7.2.3_@babel_fc7c182c64d09662684b10b18db5ed4a/node_modules/react-notion-x/src/styles.css ***!
+  \************************************************************************************************************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
